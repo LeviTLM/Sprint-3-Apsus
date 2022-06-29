aebb4a0c8aa8bcf61806fbac5f172ec19be7dab2
leviTLM <levi.talmazan@yahoo.com>
2022-06-29 13:49:29 +0300

    pull req

--- js/lib/vue-router.js
+++ js/lib/vue-router.js
@@ -1,8 +1,4 @@
-/*!
-  * vue-router v3.5.3
-  * (c) 2021 Evan You
-  * @license MIT
-  */
+
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :

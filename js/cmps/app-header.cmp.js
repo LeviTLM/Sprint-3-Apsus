d14a37547e967bbebf520280598a18a75dc4e38c
leviTLM <levi.talmazan@yahoo.com>
2022-06-30 10:34:47 +0300

    more css and some html fixing

--- js/cmps/app-header.cmp.js
+++ js/cmps/app-header.cmp.js
@@ -1,23 +1,83 @@
+// export default {
+//     template: `
+//  <header class="app-header">
+   
+//     <!-- <nav> 
+//         <section> 
+//             <!-- router link here when its ready -->
+//                 <div class="logo">
+                    // <!-- NEXT
+//                 <img src="img/logo.png">
+//             </div>
+//             </router-link>
+//         </section>
+//         <section class="nav-links"></section> 
+     
+//       <router-link class="header-link" to ="/book">Books</router-link>
+//         <!-- router link here when its ready -->
+//         <!-- <h2>email</h2>
+//         router link here when its ready
+//         <h3>notes</h3>
+//         </nav>
+//     </header> --> --> -->
+// `,
+// }
+
+
+// export default {
+//     template: `
+//     <nav class="navbar">
+//         <div class="navbar-cotainer">
+//             <ac id="navbar-logo">Appsus</a>
+//             <!-- router link here when its ready -->
+//          <!-- <a id="navbar-logo"><img class="main-logo"src="img/logo.png"></a> -->
+//             <div class="navbar-toggle" id="mobile-menu">
+//                 <span class="bar"></span>
+//                 <span class="bar"></span>
+//                 <span class="bar"></span>
+//             </div>
+//             <ul class="navbar-menu">
+//                 <li class="navbar-item">
+//                 <router-link class="header-link" to ="/book">Books</router-link>
+//                 </li>
+//                 <li class="navbar-item">
+//                     <a class="navbar-links">
+//                         <!-- router link here when its ready -->
+//                         Email
+//                     </a>
+// </li>
+// <li class="navbar-item">
+//                     <a class="navbar-links">
+//                         <!-- router link here when its ready -->
+//                         Notes
+//                     </a>
+// </li>
+//             </ul>
+//         </div>
+//     </nav>
+//     `
+// }
+
 export default {
     template: `
- <header class="app-header">
-   
-    <nav> 
-        <section> 
-            <!-- router link here when its ready -->
-                <div class="logo">
-                <img src="img/logo.png">
-            </div>
-            </router-link>
-        </section>
-        <section class="nav-links"></section>
-     
-      <router-link class="header-link" to ="/book">Books</router-link>
-        <!-- router link here when its ready -->
-        <h2>email</h2>
-        <!-- router link here when its ready -->
-        <h3>notes</h3>
+    <header class="app-header">
+        <nav>
+            <section>
+                <router-link to="/" active-class="active-link" exact title="Home Page">
+                    <div class="logo">
+                        <img src="img/logo.png">
+                    </div>
+                </router-link>
+            </section>
+            <section class = "nav-links">
+                <router-link active-class="active" class="header-link" to="/book">Books </router-link>|
+                <!-- router link here when its ready -->
+                <p>Emails</p>
+                <!-- router link here when its ready -->
+                <p>Notes</p>
+                
+            </section>
         </nav>
     </header>
-`,
+    `,
 }
--- js/pages/app-home.cmp.js
+++ js/pages/app-home.cmp.js
@@ -1,27 +1,24 @@
 export default {
     template: `
     <section class="main-home"> 
-        <div class="main-home img">
-            <img src="img/logo.png">
-        </div>
 </div>
         <div class="main-content">
             <div class="main-email">
                 <!-- router link here when its ready  -->
-                <p> Click here to send emails if you want :)
+                <p> 
                 </p>
                 <!-- router link here when its ready  -->
 </div>
 
             <div class="main-books">
                 <!-- router link here when its ready  -->
-                <p> read books here ya nerd 
+                <p> 
                 </p>
                 <!-- router link here when its ready  -->
             </div>
             <div class="main-notes">
                 <!-- router link here when its ready  -->
-                <p> schedule your day here :)
+                <p> 
                 </p>
                 <!-- router link here when its ready  -->
             </div>
